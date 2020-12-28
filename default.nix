67b293134d36012f8d8da6ce20e1f28d7c2b0294
Henri Menke <henri@henrimenke.de>
2020-12-28 16:31:02 +0100

    Use overlay properly

--- default.nix
+++ default.nix
@@ -1,11 +1,15 @@
-{ pkgs ? (import (
-    let
-      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-    in
-    with lock.nodes.nixpkgs.locked; fetchTarball {
-      url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
-      sha256 = narHash;
-    }
-  ) {})
+{ pkgs ? (import
+    (
+      let
+        lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+      in
+      with lock.nodes.nixpkgs.locked; fetchTarball {
+        url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+        sha256 = narHash;
+      }
+    )
+    { overlays = [ (import ./overlay.nix) ]; })
 }:
-(import ./overlay.nix null pkgs).dmftPackages
+(if pkgs ? dmftPackages
+then pkgs
+else pkgs.appendOverlays [ (import ./overlay.nix) ]).dmftPackages
