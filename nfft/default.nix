50f59e4f80aca850a4dd2b9fa9ed5e82bffc2446
Henri Menke <henri@henrimenke.de>
2021-02-24 21:24:49 +0100

    darwin: Fix nfft

--- nfft/default.nix
+++ nfft/default.nix
@@ -17,6 +17,7 @@ stdenv.mkDerivation rec {
     rev = version;
     sha256 = "sha256:1liphapa64c16pw3dz2mqdq31jv5r01prdh1289616cvb1kp4232";
   };
+  patches = [ ./ac-check-decls.patch ];
 
   preConfigure = ''
     bash bootstrap.sh
