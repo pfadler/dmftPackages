7036ab4f05256fc5851c443bd000bac99cd2fe3c
Henri Menke <henri@henrimenke.de>
2021-06-04 09:39:21 +0200

    Update nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Packages for DMFT calculations";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils, ... }:
