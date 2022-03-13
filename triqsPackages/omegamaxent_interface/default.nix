47f62004e2f7e57154fb54e325df6ca7aedf16d3
Henri Menke <henri@henrimenke.de>
2022-03-13 18:54:57 +0100

    triqsPackages/omegamaxent_interface: 3.0.0 -> 3.1.0

--- triqsPackages/omegamaxent_interface/default.nix
+++ triqsPackages/omegamaxent_interface/default.nix
@@ -12,17 +12,18 @@
 
 stdenv.mkDerivation rec {
   pname = "omegamaxent_interface";
-  version = "3.0.0";
+  version = "3.1.0";
 
   src = fetchFromGitHub {
     owner = "TRIQS";
     repo = pname;
     rev = version;
-    sha256 = "sha256-JeqozCcqicurOSyoPepHYYzbHR4eoSCDgMfRkmliUug=";
+    sha256 = "sha256-k6SsNuYFOmxN0K/jysYzA3ONHR2QIzFDe+f1Jjulngo=";
   };
   patches = [ ./omegamaxent_interface.patch ];
 
   nativeBuildInputs = [ cmake gtest ];
+  cmakeFlags = [ "-DBuild_Deps=Never" ];
   buildInputs = [ fftw gsl openblasCompat ];
   propagatedBuildInputs = [ triqsPackages.triqs ];
 
