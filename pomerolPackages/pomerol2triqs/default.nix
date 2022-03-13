fbfaf54d7f35f9bbd1a77fdefd3df5d8a8da6dcd
Henri Menke <henri@henrimenke.de>
2022-03-13 20:39:27 +0100

    pomerolPackages/pomerol2triqs: 0.5+git20201127.719e69a -> 0.7

--- pomerolPackages/pomerol2triqs/default.nix
+++ pomerolPackages/pomerol2triqs/default.nix
@@ -2,44 +2,27 @@
 , lib
 , fetchFromGitHub
 , cmake
-, gtest
-, boost
 , pomerolPackages
 , triqsPackages
 , ncurses
 , mpi
 , openssh
 }:
-let
-  rev = "719e69a23bbee0de69fdae0cacc452227241e06e";
-in
+
 stdenv.mkDerivation rec {
   pname = "pomerol2triqs";
-  version = "0.5+git20201127.${builtins.substring 0 7 rev}";
+  version = "0.7";
 
   src = fetchFromGitHub {
     owner = "krivenko";
     repo = pname;
-    inherit rev;
-    sha256 = "sha256-i6h8kJzpu7u+SvefZaGZOfNrVgAjUL7h7Svndk9mBNA=";
+    rev = "v${version}";
+    sha256 = "sha256-na/jDQkbSaT36VXu02Xg1y8DvUeKH2gf46frkvW16iU=";
   };
-  patches = [ ./fconcepts.patch ];
 
   nativeBuildInputs = [ cmake ];
   cmakeFlags = [ "-DCMAKE_SKIP_BUILD_RPATH=OFF" ];
-  buildInputs =
-    let
-      boostWithMpi = boost.override {
-        useMpi = true;
-        inherit mpi;
-      };
-    in
-    [
-      boostWithMpi
-      mpi
-      ncurses
-      pomerolPackages.pomerol
-    ];
+  buildInputs = [ mpi ncurses pomerolPackages.pomerol ];
   propagatedBuildInputs = [ triqsPackages.triqs ];
 
   OMPI_MCA_rmaps_base_oversubscribe = "yes";
