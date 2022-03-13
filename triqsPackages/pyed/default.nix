3c47e7846370c6838aa443901a58512c85035611
Henri Menke <henri@henrimenke.de>
2022-03-13 18:58:14 +0100

    triqsPackages/pyed: 1.0+git20210122.e6a5b7d -> 1.0+git20220208.e2952c2

--- triqsPackages/pyed/default.nix
+++ triqsPackages/pyed/default.nix
@@ -4,24 +4,27 @@
 , numpy
 , scipy
 , triqsPackages
+, pytestCheckHook
 }:
 let
-  rev = "e6a5b7db20d3cc94383e29c9050ffc47267a8584";
+  rev = "e2952c250f1505df54c39d150db4be9e44b3e012";
 in
 buildPythonPackage rec {
   pname = "pyed";
-  version = "1.0+git20210122.${builtins.substring 0 7 rev}";
+  version = "1.0+git20220208.${builtins.substring 0 7 rev}";
 
   src = fetchFromGitHub {
     owner = "HugoStrand";
     repo = pname;
     inherit rev;
-    sha256 = "sha256-6yyGhnvg8aYDi0EYdYfD+IJANLfaUIsyiWPXlbMDvjM=";
+    sha256 = "sha256-smQ7IdHTH02ZJMbvtE0clqy23eGyw0OdFRUVPfWm9yQ=";
   };
 
+  patches = [ ./fix-tab-error.patch ];
   propagatedBuildInputs = [ numpy scipy triqsPackages.triqs ];
 
-  doCheck = false;
+  checkInputs = [ pytestCheckHook ];
+  doCheck = true;
 
   meta = {
     description = "Exact diagonalization for finite quantum systems";
