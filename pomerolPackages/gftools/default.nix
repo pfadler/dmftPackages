25e0aecd8da316e90cb357c05ad80053dfd399c0
Henri Menke <henri@henrimenke.de>
2022-03-13 19:21:16 +0100

    pomerolPackages/gftools: 1.1 -> 1.1.2+git20201027.d79810d

--- pomerolPackages/gftools/default.nix
+++ pomerolPackages/gftools/default.nix
@@ -8,15 +8,18 @@
 , openssh
 }:
 
+let
+  rev = "d79810dd705b8e2efa802321382ebb7658e5f452";
+in
 stdenv.mkDerivation rec {
   pname = "gftools";
-  version = "1.1";
+  version = "1.1.2+git20201027.${builtins.substring 0 7 rev}";
 
   src = fetchFromGitHub {
     owner = "aeantipov";
     repo = pname;
-    rev = version;
-    sha256 = "sha256-FFdyIPnJLfhp59QPWUyOiNDoIndifAorRqWPfT0pC4Q=";
+    inherit rev;
+    sha256 = "sha256-UW64iWNu+ixkCC+0agdd9l09hT1z4DlBe6DAz2fpSXI=";
   };
 
   patches = [ ./gftools.patch ];
