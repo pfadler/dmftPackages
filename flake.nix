2601e4601f99fae97f0440b68ebc93e7bac59557
Henri Menke <henri@henrimenke.de>
2022-03-13 17:50:06 +0100

    flake.lock: Update
    Flake lock file changes:
    
    • Updated input 'flake-utils':
        'github:numtide/flake-utils/74f7e4319258e287b0f9cb95426c9853b282730b' (2021-11-28)
      → 'github:numtide/flake-utils/3cecb5b042f7f209c56ffd8371b2711a290ec797' (2022-02-07)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/96b4157790fc96e70d6e6c115e3f34bba7be490f' (2021-11-30)
      → 'github:NixOS/nixpkgs/bacbfd713b4781a4a82c1f390f8fe21ae3b8b95b' (2022-03-10)

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   description = "Packages for DMFT calculations";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils, ... }:
