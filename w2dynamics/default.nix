1fb8d087755e8ef19ba5055db12b736bb2bea676
Henri Menke <henri@henrimenke.de>
2022-08-23 17:49:58 +0200

    w2dynamics: backport fixes for recent NumPy

--- w2dynamics/default.nix
+++ w2dynamics/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , lib
 , fetchFromGitHub
+, fetchpatch
 , cmake
 , gfortran
 , openblasCompat
@@ -20,6 +21,14 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     sha256 = "sha256-4py4xBVD6kN8D/If+bXn+4OjwI1n6m1M1puZbA3fBbE=";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "Rename-end-to-endp-in-CTQMC.F90-as-workaround-for-f2py";
+      url = "https://github.com/w2dynamics/w2dynamics/commit/d02d8c9973fec34540061ea3ec5705bbd25bdc5b.patch";
+      sha256 = "sha256-gszDgaONdRXGdL2dJa4TukFubxO1Wel4V34pTu1RSdw=";
+    })
+  ];    
 
   nativeBuildInputs = [ cmake python3Packages.wrapPython ];
 
