90a688cb76cf7f1f9808e1e8c702d30d5c8b6f4d
Henri Menke <henri@henrimenke.de>
2022-08-23 16:09:28 +0200

    triqsPackages/tprf: backport fixes for recent NumPy

--- triqsPackages/tprf/default.nix
+++ triqsPackages/tprf/default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , lib
 , fetchFromGitHub
+, fetchpatch
 , cmake
 , gtest
 , triqsPackages
@@ -19,7 +20,19 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-+bZ/FZT//KD77iXKidH5nROIfBHsgZ4Od2Gx8CqRDqM=";
   };
 
-  patches = [ ./tprf.patch ];
+  patches = [
+    (fetchpatch {
+      name = "py-fix-numpy-depr-warnings-errors-with-modern-numpy";
+      url = "https://github.com/TRIQS/tprf/commit/f3130a3aa8c6ea2380cea1c825af2c467e614a1f.patch";
+      sha256 = "sha256-Z2FCeJ332d/a/aBe6hpoaPnkUa9+Q47zImb3wVoxs4w=";
+    })
+    (fetchpatch {
+      name = "py-one-more-numpy-depr-fix";
+      url = "https://github.com/TRIQS/tprf/commit/b7edb9b67081482f18953d032953b8aaf31dd1bc.patch";
+      sha256 = "sha256-iERnBVnu/CKPUS08Ddzt+y8lUmSQr4/1M5vstCVePKA=";
+    })
+    ./tprf.patch
+  ];
   nativeBuildInputs = [ cmake gtest ];
   cmakeFlags = [ "-DBuild_Deps=Never" ];
   buildInputs = [ ncurses ];
