8f3e3bc2ed5a2d56d23599ea2826035b932c781c
Henri Menke <henri@henrimenke.de>
2022-09-25 17:41:50 +0200

    triqsPackages.tprf: 3.1.0 -> 3.1.1

--- triqsPackages/tprf/default.nix
+++ triqsPackages/tprf/default.nix
@@ -11,28 +11,16 @@
 
 stdenv.mkDerivation rec {
   pname = "tprf";
-  version = "3.1.0";
+  version = "3.1.1";
 
   src = fetchFromGitHub {
     owner = "TRIQS";
     repo = pname;
     rev = version;
-    sha256 = "sha256-+bZ/FZT//KD77iXKidH5nROIfBHsgZ4Od2Gx8CqRDqM=";
+    sha256 = "sha256-zgSX1sliQ6QYNx/D1WZ6cRuYHN/UJYnQfOVi3WDda4k=";
   };
 
-  patches = [
-    (fetchpatch {
-      name = "py-fix-numpy-depr-warnings-errors-with-modern-numpy";
-      url = "https://github.com/TRIQS/tprf/commit/f3130a3aa8c6ea2380cea1c825af2c467e614a1f.patch";
-      sha256 = "sha256-Z2FCeJ332d/a/aBe6hpoaPnkUa9+Q47zImb3wVoxs4w=";
-    })
-    (fetchpatch {
-      name = "py-one-more-numpy-depr-fix";
-      url = "https://github.com/TRIQS/tprf/commit/b7edb9b67081482f18953d032953b8aaf31dd1bc.patch";
-      sha256 = "sha256-iERnBVnu/CKPUS08Ddzt+y8lUmSQr4/1M5vstCVePKA=";
-    })
-    ./tprf.patch
-  ];
+  patches = [ ./tprf.patch ];
   nativeBuildInputs = [ cmake gtest ];
   cmakeFlags = [ "-DBuild_Deps=Never" ];
   buildInputs = [ ncurses ];
