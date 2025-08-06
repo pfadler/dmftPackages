f377924cc60ebe7406f3abdc5a52ee7d05cdb6cb
Paul Fadler <paul.fadler@fau.de>
2025-08-06 22:57:50 +0200

    new version cpp2py

--- triqsPackages/cpp2py/default.nix
+++ triqsPackages/cpp2py/default.nix
@@ -7,17 +7,17 @@
 }:
 
 let
-  rev = "b268654da7a06fa526be58382b4f423b84bf5fe2";
+  rev = "d5663cfbefc5302ca1e6d3292cd019f41c81966e";
 in
 stdenv.mkDerivation rec {
   pname = "cpp2py";
-  version = "2.0.0+git20220304.${builtins.substring 0 7 rev}";
+  version = "2.0.0+git20241109.${builtins.substring 0 7 rev}";
 
   src = fetchFromGitHub {
     owner = "TRIQS";
     repo = pname;
     inherit rev;
-    sha256 = "sha256-69UG1T+CRG3+fWGO5peJhGQrT1MYmt1Z64/7GCk2Vi8=";
+    sha256 = "sha256-OidPEcfOIIFEFoQEeCsPwxGWQNCSBG8ESesw/688Bw4=";
   };
 
   nativeBuildInputs = [ cmake python3Packages.wrapPython ];
