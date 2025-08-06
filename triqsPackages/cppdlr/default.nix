8eb681855d88469650ae817802a31140416b867a
Paul Fadler <paul.fadler@fau.de>
2025-08-06 23:02:07 +0200

    add cppdlr

---
+++ triqsPackages/cppdlr/default.nix
@@ -0,0 +1,50 @@
+{ stdenv
+, lib
+, fetchFromGitHub
+, cmake
+, gtest
+, triqsPackages
+, fmt
+, python3Packages
+, openblasCompat
+, hdf5
+, mpi
+, git
+}:
+
+stdenv.mkDerivation rec {
+  pname = "cppdlr";
+  version = "1.2.0";
+
+  src = fetchFromGitHub {
+    owner = "flatironinstitute";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-6ZyaIAgg5Ik3IodWVZavclm1LwGLqlYPIplHjZUd8eI=";
+  };
+
+  patches = [ ./cppdlr.patch ];
+  cmakeFlags = [ "-DBuild_Deps=Never" ];
+  nativeBuildInputs = [ cmake gtest git ];
+  buildInputs = [
+    hdf5
+    mpi
+    openblasCompat
+    fmt
+  ];
+
+  propagatedBuildInputs = [
+    triqsPackages.cpp2py
+    triqsPackages.h5
+    triqsPackages.nda
+    triqsPackages.itertools
+    triqsPackages.mpi
+  ];
+
+  meta = {
+    description = "Discrete Lehmann representation of imaginary time Green's functions";
+    homepage = "https://github.com/flatironinstitute/cppdlr";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ hmenke ];
+  };
+}
