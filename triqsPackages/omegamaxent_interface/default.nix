42a0dfae89f4a621e3dcd20f4029f1dcfbb9eee0
Paul Fadler <paul.fadler@fau.de>
2025-08-11 20:07:58 +0200

    new version for omegamaxent interface

--- triqsPackages/omegamaxent_interface/default.nix
+++ triqsPackages/omegamaxent_interface/default.nix
@@ -8,23 +8,24 @@
 , openblasCompat
 , triqsPackages
 , openssh
+, boost
 }:
 
 stdenv.mkDerivation rec {
   pname = "omegamaxent_interface";
-  version = "3.1.0";
+  version = "3.3.0";
 
   src = fetchFromGitHub {
     owner = "TRIQS";
     repo = pname;
     rev = version;
-    sha256 = "sha256-k6SsNuYFOmxN0K/jysYzA3ONHR2QIzFDe+f1Jjulngo=";
+    sha256 = "sha256-2FyrBluCN8Mqp7UZhbQb1Ryjva+5p8vgCN3cUtisl20=";
   };
   patches = [ ./omegamaxent_interface.patch ];
 
   nativeBuildInputs = [ cmake gtest ];
   cmakeFlags = [ "-DBuild_Deps=Never" ];
-  buildInputs = [ fftw gsl openblasCompat ];
+  buildInputs = [ fftw gsl openblasCompat boost ];
   propagatedBuildInputs = [ triqsPackages.triqs ];
 
   enableParallelChecking = false;
