e70700073510b8f1742cb8c3e2fab55cf4dafb70
Paul Fadler <paul.fadler@fau.de>
2025-08-11 20:07:29 +0200

    pyed new release

--- triqsPackages/pyed/default.nix
+++ triqsPackages/pyed/default.nix
@@ -6,19 +6,18 @@
 , triqsPackages
 , pytestCheckHook
 }:
-let
-  rev = "6359be4f287bddd67db3a50960880de6b645f008";
-in
 buildPythonPackage rec {
   pname = "pyed";
-  version = "1.0+git20220317.${builtins.substring 0 7 rev}";
+  version = "0.0.1";
 
   src = fetchFromGitHub {
     owner = "HugoStrand";
     repo = pname;
-    inherit rev;
-    sha256 = "sha256-/jHUbN6oEfSYRjJf8ap1xF4a5Vz27lNLaqM/RV8G4Sg=";
+    rev = version;
+    sha256 = "sha256-/i2h56ISQqUIVXfSclgRWq4mgv9yzdqb9KpHyEBJZXw=";
   };
+
+  patches = [ ./pyed.patch ];
 
   propagatedBuildInputs = [ numpy scipy triqsPackages.triqs ];
 
