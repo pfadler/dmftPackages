65ba207165b2c5c6c1701e190ccd0d8b2a9e83c7
Paul Fadler <paul.fadler@fau.de>
2025-09-28 19:35:26 +0200

    new version

--- pomerolPackages/libcommute/default.nix
+++ pomerolPackages/libcommute/default.nix
@@ -8,32 +8,15 @@
 
 stdenv.mkDerivation rec {
   pname = "libcommute";
-  version = "0.7.1";
+  version = "0.7.2";
 
   src = fetchFromGitHub {
     owner = "krivenko";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-wpA0Rnv8ROku7ikncCby7rftFDUeNxO/AONS/0/ibLg=";
+    sha256 = "sha256-BGn5q6sqVqWKiZDEqWw1ZLCAVoY2gIHFyx00BB3HGmE=";
   };
 
-  patches = [
-    (fetchpatch {
-      name = "fix-install-CMake-config-in-subdirectory.patch";
-      url = "https://github.com/krivenko/libcommute/commit/2e3202a278efc938be7d1241becc9d3f26d3f03e.patch";
-      sha256 = "sha256-LLdV55Jd6V4tIQN+glxb5MqP7BdyjpXEct6cjP0kfBg=";
-    })
-    (fetchpatch {
-      name = "doc-Consistently-use-lowercased-libcommute-in-CMake-.patch";
-      url = "https://github.com/krivenko/libcommute/commit/863b67380742b4431b9170fbcdf51ff81172db0e.patch";
-      sha256 = "sha256-+Gb8K/xjPnTmS8MYVSzimLS00rZJHY89kkNOzSOKryI=";
-    })
-    (fetchpatch {
-      name = "test-Upgrade-bundled-Catch2-to-v2.13.9-fix-2.patch";
-      url = "https://github.com/krivenko/libcommute/commit/98624b3b57875711f5958ee168e65d809c31bcbb.patch";
-      sha256 = "sha256-1IuVKH1+7+ETDg+cebtbMySyvJs1d9x6ferxBBgn9pg=";
-    })
-  ];
   nativeBuildInputs = [ cmake ];
   buildInputs = [ eigen ];
 
