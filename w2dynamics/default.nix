190851ecbfe1ea419bb4f8e7ac493d9abe558647
Paul Fadler <paul.fadler@fau.de>
2025-09-28 19:04:45 +0200

    New version necessitates fftwf

--- w2dynamics/default.nix
+++ w2dynamics/default.nix
@@ -6,6 +6,7 @@
 , gfortran
 , openblasCompat
 , fftw
+, fftwFloat
 , nfft
 , python3
 , python3Packages
@@ -13,22 +14,14 @@
 
 stdenv.mkDerivation rec {
   pname = "w2dynamics";
-  version = "1.1.3";
+  version = "1.1.6";
 
   src = fetchFromGitHub {
     owner = "w2dynamics";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256-4py4xBVD6kN8D/If+bXn+4OjwI1n6m1M1puZbA3fBbE=";
+    sha256 = "sha256-sBLSui+FFn5js+Ri6kcXcOEv5J/3Z0cXrhGlHdLFLnA=";
   };
-
-  patches = [
-    (fetchpatch {
-      name = "Rename-end-to-endp-in-CTQMC.F90-as-workaround-for-f2py";
-      url = "https://github.com/w2dynamics/w2dynamics/commit/d02d8c9973fec34540061ea3ec5705bbd25bdc5b.patch";
-      sha256 = "sha256-gszDgaONdRXGdL2dJa4TukFubxO1Wel4V34pTu1RSdw=";
-    })
-  ];
 
   nativeBuildInputs = [ cmake python3Packages.wrapPython ];
 
@@ -36,7 +29,7 @@ stdenv.mkDerivation rec {
 
   enableParallelBuilding = false;
 
-  buildInputs = [ gfortran openblasCompat fftw nfft ];
+  buildInputs = [ gfortran openblasCompat fftw fftwFloat nfft ];
 
   pythonPath = with python3Packages; [ numpy scipy h5py mpi4py configobj ];
   propagatedBuildInputs = pythonPath;
