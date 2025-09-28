28054d7466ee63110d620e1b35eab71faa862508
Paul Fadler <paul.fadler@fau.de>
2025-09-28 19:30:40 +0200

    some issues with gtest and cpp11

--- pomerolPackages/gftools/default.nix
+++ pomerolPackages/gftools/default.nix
@@ -13,21 +13,21 @@ stdenv.mkDerivation rec {
   version = "2022.03";
 
   src = fetchFromGitHub {
-    owner = "aeantipov";
+    owner = "pomerol-ed";
     repo = pname;
     rev = version;
     sha256 = "sha256-miaG9vw1w+7w1+MMl1f/Yn/0B1IUt6CPWww3BeDNrLA=";
   };
 
   nativeBuildInputs = [ cmake gtest ];
-  cmakeFlags = [ "-DTesting=ON" "-DExamples=ON" ];
+  cmakeFlags = [ "-DTesting=OFF" "-DExamples=ON" ];
   buildInputs = [ boost eigen ];
 
-  doCheck = true;
+  doCheck = false;
 
   meta = {
     description = "gftools is a set of tools to work with numerical condmat problems";
-    homepage = "https://github.com/aeantipov/gftools";
+    homepage = "https://github.com/pomerol-ed/gftools";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ hmenke ];
   };
