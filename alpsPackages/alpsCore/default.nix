e07dd7d231d24cadb48cd5a90a615af1b04e2910
Paul Fadler <paul.fadler@fau.de>
2025-09-29 20:53:00 +0200

    new alps version

--- alpsPackages/alpsCore/default.nix
+++ alpsPackages/alpsCore/default.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   pname = "ALPSCore";
-  version = "2.2.0";
+  version = "2.3.2";
 
   src = fetchFromGitHub {
     owner = "ALPSCore";
     repo = pname;
     rev = "v${version}";
-    sha256 = "sha256:0brnbly6p227x2lw28rlx9wl64yff3x13wfy9kwl9jjil97r6ryl";
+    sha256 = "sha256-0fsFxA2nB5XQezPLolGD0ePzicJZEasvlxNE3cnON30=";
   };
 
   nativeBuildInputs = [ cmake ];
